e1fc7f720fc94c9ae05c08c3fdbb8ca28b108939
iksan <iksangame80@gmail.com>
2022-07-03 09:22:12 +0700

    melakukan perubahan pada file liking movie

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -32,38 +32,3 @@ Scenario('liking one restaurant', async ({ I }) => {
 
   assert.strictEqual(firstRestoTitle, likedRestoTitle);
 });
-
-Scenario('searching restaurants', async ({ I }) => {
-  I.see('Tidak ada resto untuk ditampilkan', '.restaurant-item__not__found');
-
-  I.amOnPage('/');
-
-  I.seeElement('.restaurant__title a');
-
-  const titles = [];
-
-  for (let i = 1; i <= 3; i++) {
-    I.click(locate('.restaurant__title a').at(i));
-    I.seeElement('#likeButton');
-    I.click('#likeButton');
-    titles.push(await I.grabTextFrom('.restaurant__title'));
-    I.amOnPage('/');
-  }
-
-  I.amOnPage('/#/favorite');
-  I.seeElement('#query');
-
-  const searchQuery = titles[1].substring(1, 3);
-  const matchingRestaurants = titles.filter((title) => title.indexOf(searchQuery) !== -1);
-
-  I.fillField('#query', searchQuery);
-  I.pressKey('Enter');
-
-  const visibleLikedRestaurants = await I.grabNumberOfVisibleElements('.restaurant-item');
-  assert.strictEqual(matchingRestaurants.length, visibleLikedRestaurants);
-
-  matchingRestaurants.forEach(async (title, index) => {
-    const visibleTitle = await I.grabTextFrom(locate('.restaurant__title').at(index + 1));
-    assert.strictEqual(title, visibleTitle);
-  });
-});
